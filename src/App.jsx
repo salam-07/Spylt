87767d6b0439fc979048da0180da52b80690ecc6
salam-07 <abdussalamblue7@gmail.com>
2026-08-13 21:24:26 +0500

    Add Navbar, HeroSection, and MessageSection components with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './sections/HeroSection';
+import { ScrollTrigger } from 'gsap/all';
+import gsap from 'gsap';
+import MessageSection from './sections/MessageSection';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Navbar />
+      <HeroSection />
+      <MessageSection />
+      <div className='min-h-dvh border'></div>
+    </main>
   );
 };
 
