b0aa81b2470869777d23ea7db63ee19f5c3488d0
salam-07 <abdussalamblue7@gmail.com>
2026-08-15 12:35:08 +0500

    Add BenefitSection and ClipPathTitle components with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MessageSection from './sections/MessageSection';
 import FlavorSection from './sections/FlavorSection';
 import { useGSAP } from '@gsap/react';
 import NutritionSection from './sections/NutritionSection';
+import BenefitSection from './sections/BenefitSection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -27,6 +28,7 @@ const App = () => {
           <MessageSection />
           <FlavorSection />
           <NutritionSection />
+          <BenefitSection />
           <div className="min-h-dvh border"></div>
         </div>
       </div>
